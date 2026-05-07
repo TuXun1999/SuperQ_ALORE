8c40b788778473d27a8f295ea58a281d96be4748
Marvrobo <138975686+Marvrobo@users.noreply.github.com>
2026-05-07 07:18:14 -0500

    added multiple assets spawn logic and set random_choice to false to ensure assets are spawned in order

--- source/SuperQ_ALORE/SuperQ_ALORE/tasks/manager_based/superq_alore/mdp/scene.py
+++ source/SuperQ_ALORE/SuperQ_ALORE/tasks/manager_based/superq_alore/mdp/scene.py
@@ -2,6 +2,7 @@ import isaaclab.sim as sim_utils
 from isaaclab.assets import RigidObjectCfg
 from isaaclab.assets import ArticulationCfg
 from SuperQ_ALORE.assets import ASSET_DIR
+from SuperQ_ALORE.assets.object_catalog import OBJECT_CATALOG
 # Default configurations for the object
 DEFAULT_RIGID_PROPS = sim_utils.RigidBodyPropertiesCfg(
     rigid_body_enabled=True,
@@ -37,24 +38,49 @@ GROUND_PATCH_CFG = RigidObjectCfg(
     init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, -0.5 * GROUND_PATCH_THICKNESS)),
 )
 
-# The chair
-# CHAIR_USD_PATH = ASSET_DIR + "/objects/Shorter_Office_Chair.usd" 
-# CHAIR_USD_PATH = ASSET_DIR + "/objects/armchair_with_contact_sensor.usda" 
-# CHAIR_USD_PATH = ASSET_DIR + "/objects/meramic_chair_with_contact_sensor.usd"
-# CHAIR_USD_PATH = ASSET_DIR + "/objects/new_shorter_office_chair.usd"
-# CHAIR_USD_PATH = ASSET_DIR + "/objects/birch_seat.usd"
-CHAIR_USD_PATH = ASSET_DIR + "/objects/willow_bench.usd"
+
+# previous: single chair 
+# # The chair
+# # CHAIR_USD_PATH = ASSET_DIR + "/objects/Shorter_Office_Chair.usd" 
+# # CHAIR_USD_PATH = ASSET_DIR + "/objects/armchair_with_contact_sensor.usda" 
+# # CHAIR_USD_PATH = ASSET_DIR + "/objects/meramic_chair_with_contact_sensor.usd"
+# # CHAIR_USD_PATH = ASSET_DIR + "/objects/new_shorter_office_chair.usd"
+# # CHAIR_USD_PATH = ASSET_DIR + "/objects/birch_seat.usd"
+# CHAIR_USD_PATH = ASSET_DIR + "/objects/willow_bench.usd"
 
 
-CHAIR_RIGID_CFG = RigidObjectCfg(
-    prim_path="{ENV_REGEX_NS}/Pushable",
-    spawn=sim_utils.UsdFileCfg(
-        usd_path=CHAIR_USD_PATH,
+# CHAIR_RIGID_CFG = RigidObjectCfg(
+#     prim_path="{ENV_REGEX_NS}/Pushable",
+#     spawn=sim_utils.UsdFileCfg(
+#         usd_path=CHAIR_USD_PATH,
+#         rigid_props=DEFAULT_RIGID_PROPS,
+#         collision_props=DEFAULT_COLLISION_PROPS,
+#         mass_props=sim_utils.MassPropertiesCfg(mass=0.5),
+
+# Multi-object target assets loaded from OBJECT_CATALOG.
+_CATALOG_ASSETS_CFG = tuple(
+    sim_utils.UsdFileCfg(
+        usd_path=entry.asset_path,
         rigid_props=DEFAULT_RIGID_PROPS,
         collision_props=DEFAULT_COLLISION_PROPS,
         mass_props=sim_utils.MassPropertiesCfg(mass=0.5),
+    )
+    for entry in OBJECT_CATALOG
+)
+
+# we set the random_choice to False so that asset configurations are spawned in the order
+# defined in OBJECT_CATALOG, which allows us to have a deterministic mapping from env index to object 
+# (i.e. env i will always have OBJECT_CATALOG[i % N], where N is the number of objects in the catalog)
+CATALOG_MULTI_OBJECT_RIGID_CFG = RigidObjectCfg(
+    prim_path="{ENV_REGEX_NS}/Pushable",
+    spawn=sim_utils.MultiAssetSpawnerCfg(
+        assets_cfg=_CATALOG_ASSETS_CFG,
+        random_choice=False,
     ),
+
+    # we reset the initail pose in event.py: reset_target_object_from_catalog_pose
     init_state=RigidObjectCfg.InitialStateCfg(
-        pos=(0.15, 0.0, 0.0),
-        rot=(0.707, 0.0, 0.0, 0.707)),
+        pos=(0.0, 0.0, 0.0),
+        rot=(1.0, 0.0, 0.0, 0.0),
+    ),
 )
